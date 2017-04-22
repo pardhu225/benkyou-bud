e7745737c33a466eebf4d9fc730329d13c04244f
Pardhu Chowdary <Pardhu Chowdary>
2017-04-23 00:45:52 +0530

    some advancements to the home page

--- index.php
+++ index.php
@@ -32,7 +32,24 @@
 		}
 		body {
 			padding: 0px;
-			margin:0px;
+			margin:0px 10%;
+			font-family: sans-serif;
+		}
+		#left-pane {
+			padding:0.7em 0.3em;
+		}
+		#right-pane {
+			padding-left: 0.5em;
+			border-left: solid 2px violet;
+		}
+		footer {
+			text-align: center;
+			padding-top: 2em;
+			border-top: solid 2px violet;
+		}
+		input[type=text], input[type=password] {
+			height: 1.9em;
+			padding-left: 5px;
 		}
 	</style>
 </head>
@@ -43,14 +60,28 @@
 		</section>
 		<div id="content-container">
 			<section class="content-pane" id="left-pane" style="display: inline-block;position: relative;top: 0px;left: 0px">
-				This is the area where the content goes.
+				<h3>What is Benkyou-bud?</h3>
+				<p>
+					Benkyou-bud is a automated cloud software with some cool inbuilt features
+					like Course management, material sorting, date search and much more.
+				</p>
+				<h3>What makes it standout?</h3>
+				<p>
+					<h5>Its features of course</h5>
+					You can have your material sorted neat and tidy and with the on the fly search bar
+					you can view your material at anytime(literally).<br>
+					With one click you can download all the material(in the sorted manner of course!).<br>
+
+
+				</p>
+
 			</section>
 			<section class="content-pane" id="right-pane" style="display: inline-block;">
-				This is the area where the form goes
+				You may login from here:
 				<div class="form-pane">
 					<form action="processlogin.php" method="POST" id="login-form">
-						<input type="text" id="username" name="username">
-						<input type="password" id="password" name="password">
+						<input type="text" id="username" name="username" placeholder="Username">
+						<input type="password" id="password" name="password" placeholder="Password">
 						<input type="submit" value="Log in" id="login-button">
 					</form>
 					<!--
