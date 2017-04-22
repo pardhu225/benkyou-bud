88e2704e3b0cfad775f8ff153a071dce1f90980d
Pardhu Chowdary <Pardhu Chowdary>
2017-04-23 00:21:35 +0530

    Adding initial files

---
+++ README.md
@@ -0,0 +1,2 @@
+# benkyou-bud
+An automated cloud software which manages your courses and all the stuff in there
---
+++ index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<style type="text/css">
+		.main-container {
+			padding-left: 15%;
+			padding-right: 15%;
+			box-shadow: 5px 5px black 3px;
+
+		}
+		.content-pane {
+			padding:0	em;
+		}
+		#login-button {
+			color: white;
+			border-radius: 5px;
+			background-color: blue;
+		}
+		#left-pane {
+			min-width: 60%;
+			max-width: 60%;
+		}
+		#right-pane {
+			min-width: 30%;
+			max-width: 30%;
+			display: inline;
+		}
+		.content-container:after {
+			clear: left;
+			content: "";
+		}
+		body {
+			padding: 0px;
+			margin:0px;
+		}
+	</style>
+</head>
+<body>
+	<div id="main-container">
+		<section id="welcome-banner">
+			<img src="img/banner.jpg">
+		</section>
+		<div id="content-container">
+			<section class="content-pane" id="left-pane" style="display: inline-block;position: relative;top: 0px;left: 0px">
+				This is the area where the content goes.
+			</section>
+			<section class="content-pane" id="right-pane" style="display: inline-block;">
+				This is the area where the form goes
+				<div class="form-pane">
+					<form action="processlogin.php" method="POST" id="login-form">
+						<input type="text" id="username" name="username">
+						<input type="password" id="password" name="password">
+						<input type="submit" value="Log in" id="login-button">
+					</form>
+					<!--
+					TODO Put this form else where
+					<form action="processsignup.php" method="POST" id="signup-form">
+						<input type="text" id="name" name="name" placeholder="Your Name">
+						<input type="text" id="username" name="name" placeholder="Your preffered Username">
+						<input type="password" id="password" name="password" placeholder="Your password">
+						<input type="repassword" id="repassword" name="repassword" placeholder="Your password again">
+						<input type="text" id="email" name="email">
+						<select name="institute" id="institute">
+							TODO complete this php code
+							<option></option>
+						</select>
+						<input type="submit" value="I'm in!">
+					</form>
+					-->
+				</dib>
+			</section>
+			<div></div>
+		</div>
+		<footer>
+			Designed by: <a href="#">Pardhu</a>
+			<div id="footer-icons">
+				<a href="#"><img src="img/github-mark.png" style="height: 50px;width: 50px;"></a>
+				<a href="#"><img src="img/fbicon.png" style="height: 50px;width: 50px;"></a>
+			</div>
+		</footer>
+	</div>
+</body>
+</html>
