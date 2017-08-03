e492701988532ae38c5bdd1faffa9b4dcd862eff
pardhu225 <chowdary1969@gmail.com>
2017-08-03 15:43:38 +0530

    More improvements to the filedropper

--- dashboard/filedropper.php
+++ dashboard/filedropper.php
@@ -6,6 +6,7 @@
 <html>
 <head>
 	<title>BenkyouBud - Drop Files Here</title>
+	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 	<style>
 		body {
 			margin:0;
@@ -14,7 +15,7 @@
 		#links, #files {
 			width:50%;float:left;
 		}
-		#link-dropper {
+		#link-dropper, #file-dropper{
 			width: 80%;
 			margin: 5%;
 			padding: 5%;
@@ -22,9 +23,9 @@
 			border: solid 2px blue; 
 			text-align: center; 
 		}
-		#link-dropper img {
-			height:100px;
-			width:151px;
+		#link-dropper i, #file-dropper i {
+			font-size:10em;
+			color: mediumpurple;
 		}
 
 		#links-list ol {
@@ -37,7 +38,7 @@
 		    //text-shadow: 0 1px 0 rgba(255,255,255,.5);
 		    margin-left: 2em;
 		}
-		#links-list ol li div{
+		#links-list ol li div:first-child{
 		    position: relative;
 		    display: block;
 		    padding: .4em .4em .4em 2em;
@@ -50,16 +51,16 @@
 		    transition: all .3s ease-out;   
 		}
 
-		#links-list ol li div:hover{
+		#links-list ol li div:first-child:hover{
 		    background: #eee;
 		}
 
-		#links-list ol li div:hover:before{
+		#links-list ol li div:first-child:hover:before{
 		    //transform: rotate(360deg);  
 		    font-size: 1.2em;
 		}
 
-		#links-list ol li div:before{
+		#links-list ol li div:first-child:before{
 		    content: counter(li);
 		    counter-increment: li;
 		    position: absolute; 
@@ -84,34 +85,50 @@
 			clear: both;
 			display: block;
 		}
-		input[type=submit]:hover {
-			transition: 1s;
+		.submit span:hover{
+			transition: 0.4s;
 			background-color: lime;
 			color:white;
 		}
-		input[type=submit] {
-			display: block;
-			transition: 1s;
+		.submit span{
+			cursor: pointer;
+			display: inline;
+			width:100px;
+			transition: 0.4s;
 			height: 2em;
 			font-size: 1.2em;
 			border-radius: 1em;
-			padding-top: 0.5em;
-			padding-bottom:0.5em;
-			padding-left:2em;
-			padding-right:2em;
+			padding:0.3em 2em;
 			background-color: white;
 			border: solid 2px lime;
+		}
+		.submit {
+			text-align: right;
+			margin:2px;
+		}
+		.warn {
+			animation-name: heartbeat;
+			animation-iteration-count: infinite;
+			animation-duration: 1.5s;
+		}
+		.warn:hover {
+			animation-name: none;
+		}
+		@keyframes heartbeat {
+			0%{background-color: #bbb;}
+			50%{background-color: #eeeeee;}
+			100%{background-color: #bbb;}
 		}
 	</style>
 </head>
 <body>
 	<h2>BenkyouBud - File Dropper</h2>
 	<section id="links">
-		<section id="link-dropper"><img src="img/upload.png"><p>Drop your links here. They will be saved on your personal space</p></section>
+		<section id="link-dropper"><i class="fa fa-cloud-upload"></i><p>Drop your links here. They will be saved on your personal space</p></section>
 		<section id="links-list">
 			<ol>
 				<li>
-					<div class="body">This is the first item</div>
+					<div class="warn">This is the first item</div>
 				</li>
 				<li>
 					<div class="body">This is the first item</div>
@@ -122,14 +139,14 @@
 						<p>Inserting mulitple paragraphs to check what happerns</p>
 						<p>Inserting mulitple paragraphs to check what happerns</p>
 						<p>Inserting mulitple paragraphs to check what happerns</p>
-						<input type="submit" name="Submit">
+						<div class="submit"><span>Done...!</span></div>
 					</div>
 				</li>
 			</ol>
 		</section>
 	</section>
 	<section id="files">
-		<section id="file-dropper">File droppper here</section>
+		<section id="file-dropper"><i class="fa fa-cloud-upload"></i><p>Drop your links here. They will be saved on your personal space</p></section>
 		<section id="files=list">Files list here</section>
 	</section>
 	<script src="js/dropzone.js"></script>
